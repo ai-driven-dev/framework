220e2492b372c3e574dce40c9a170e3c8034c5d0
Baptiste LAFOURCADE <baptiste.lafourcade@gmail.com>
2026-05-12 08:25:35 +0200

    chore(framework): restore strict commitlint rules after history rewrite

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,10 +1,18 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [0, "always"],
-    "subject-case": [0, "never"],
-    "body-max-line-length": [0, "always"],
-    "footer-max-line-length": [0, "always"],
-    "header-max-length": [1, "always", 120],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "aidd-context",
+        "aidd-dev",
+        "aidd-vcs",
+        "aidd-pm",
+        "aidd-refine",
+        "aidd-orchestrator",
+        "framework",
+      ],
+    ],
   },
 };
